4d34e4fdd67ab2dbc86e4f7adec89faf2c551036
romantafelski <romantafelski18@gmail.com>
2022-09-28 12:37:23 -0400

    ;ldksjf

--- tattoos_api/inked_api/models.py
+++ tattoos_api/inked_api/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Tattoo(models.Model):
-    image = models.CharField(max_length=300)
+    image = models.CharField(max_length=3000)
     artist = models.CharField(max_length=100)
     description = models.CharField(max_length=999)
