feb9df7c068728b956e467cb8d505f87107fe16e
romantafelski <romantafelski18@gmail.com>
2022-10-10 18:42:04 -0400

    ds;lfkj

--- tattoos_api/inked_api/urls.py
+++ tattoos_api/inked_api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/tattoo', views.TattooList.as_view(), name='inked_list'), 
-    path('api/tattoo/<int:pk>', views.TattooDetail.as_view(), name='inked_detail'), 
+    path('', views.TattooList.as_view(), name='inked_list'), 
+    path('<int:pk>', views.TattooDetail.as_view(), name='inked_detail'), 
 ]
 
 
